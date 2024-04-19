9a33ed301a3c940592653bc9456b0797212279af
0088-Space <164868364+0088-Space@users.noreply.github.com>
2024-04-19 14:13:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['+255612571289', 'Rizione0088', true],
+  ['255612571289', 'Rizione0088', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
